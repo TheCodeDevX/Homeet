c19b8fb1286ab2676bafc81a08c3def1854c8bbf
Omar <omarrian3333@gmail.com>
2026-03-02 11:04:45 +0000

    fix the typo in the URL

--- client/src/lib/axios.config.ts
+++ client/src/lib/axios.config.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
  export const authApi = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}/api/auth`,
+    baseURL: `${import.meta.env.VITE_API_URL}/api/auth`,
     withCredentials: true
  })
 
@@ -11,38 +11,38 @@ import axios from 'axios'
  })
 
    export const ratingApi = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}/api/ratings`,
+    baseURL: `${import.meta.env.VITE_API_URL}/api/ratings`,
     withCredentials: true
  })
 
    export const MessageApi = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}/api/message`,
+    baseURL: `${import.meta.env.VITE_API_URL}/api/message`,
     withCredentials: true
  })
 
     export const FollowReqApi = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}/api/requests`,
+    baseURL: `${import.meta.env.VITE_API_URL}/api/requests`,
     withCredentials: true
  })
 
     export const NotifsApi = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}/api/notifications`,
+    baseURL: `${import.meta.env.VITE_API_URL}/api/notifications`,
     withCredentials: true
  })
 
     export const UserApi = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}/api/users`,
+    baseURL: `${import.meta.env.VITE_API_URL}/api/users`,
     withCredentials: true
  })
 
    export const BookingApi = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}/api/bookings`,
+    baseURL: `${import.meta.env.VITE_API_URL}/api/bookings`,
     withCredentials: true
  })
 
  
    export const AudioApi = axios.create({
-    baseURL: `${import.meta.env.VITE_BASE_URL}/api/uploading`,
+    baseURL: `${import.meta.env.VITE_API_URL}/api/uploading`,
     withCredentials: true
  })
 
